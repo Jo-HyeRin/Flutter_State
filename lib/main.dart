caa23ef7ef3a495bce2994efbf82ce8a123c720f
Jo-HyeRin <sayhyelin12@naver.com>
2022-11-14 14:44:08 +0900

    Expanded:Container 공간 확인해볼수있다!

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
 }
 
+// stateful로 HomePage 구현
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -25,7 +27,39 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(child: AComponent()),
+          Expanded(child: BComponent()),
+        ],
+      ),
+    );
   }
 }
+
+// A 컴포넌트 생성
+class AComponent extends StatelessWidget {
+  const AComponent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.yellow,
+    );
+  }
+}
+
+// B 컴포넌트 생성
+class BComponent extends StatelessWidget {
+  const BComponent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+    );
+  }
+}
+
 
