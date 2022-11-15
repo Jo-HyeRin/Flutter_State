2c2393a1b3b1b79896f0aa6486d2faa351789a05
Jo-HyeRin <sayhyelin12@naver.com>
2022-11-15 09:21:48 +0900

    ì상태관리수업 시작

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,50 +10,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
 }
 
-// stateful로 HomePage 구현
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-
-  int num = 1;
-
-  void increase(){
-    setState(() {
-      num++;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    double size = MediaQuery.of(context).size.width;
-    double screenSize = size * 0.8;
     return Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Expanded(child: AComponent(num)),
-          Expanded(child: BComponent(increase)),
+          Expanded(child: AComponent()),
+          Expanded(child: BComponent()),
         ],
       ),
     );
   }
 }
 
-// 컨슈머 (데이터 소비자)
 class AComponent extends StatelessWidget {
-  final int num;
-  const AComponent(this.num, {Key? key}) : super(key: key);
+  const AComponent({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -61,18 +41,24 @@ class AComponent extends StatelessWidget {
       color: Colors.yellow,
       child: Column(
         children: [
-          Text("AComponent"),
-          Expanded(child: Align(child: Text("${num}", style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold)))),
+          Text("ACompoent"),
+          Expanded(
+            child: Align(
+              child: Text(
+                "1",
+                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+              ),
+            ),
+          )
         ],
       ),
     );
   }
 }
 
-// 서플라이어 (데이터 공급자)
+// 서플라이어 공급자
 class BComponent extends StatelessWidget {
-  final Function increase;
-  const BComponent(this.increase, {Key? key}) : super(key: key);
+  const BComponent({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -80,14 +66,15 @@ class BComponent extends StatelessWidget {
       color: Colors.blue,
       child: Column(
         children: [
-          Text("BComponent"),
+          Text("BCompoent"),
           Expanded(
             child: Align(
               child: ElevatedButton(
-                onPressed: (){
-                  increase();
-                },
-                child: Text("숫자증가", style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold)),
+                onPressed: () {},
+                child: Text(
+                  "숫자증가",
+                  style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                ),
               ),
             ),
           ),
@@ -96,5 +83,3 @@ class BComponent extends StatelessWidget {
     );
   }
 }
-
-
